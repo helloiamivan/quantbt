ad492dc4148c1d2321b2ccf4acf992c42a9448af
helloiamivan <ivanchanzhenghao@gmail.com>
2020-01-18 22:05:11 +0000

    Add packaging abilities

---
+++ quantbt/__init__.py
@@ -0,0 +1,3 @@
+from .portfolio import *
+from .analytics import *
+from .data import *
---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='quantbt',
+    version='0.0.1',
+    description='Quantitative Research Finance',
+    author='Ivan Chan',
+    author_email='ivanchanzhenghao@gmail.com',
+    url='www.github.com/helloiamivan',
+    packages=['quantbt']
+)
+
+# Version History
+''' 0.0.1 : Alpha Development Check in '''
